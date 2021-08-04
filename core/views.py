136f45ced889b62b56c8304490459a7ad13b52ab
renatorochha <renators65x@gmail.com>
2021-08-03 22:09:12 -0300

    Commit da primeira aplicação Django. Retornando um hello world

---
+++ core/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+def hello(request, nome, idade):
+    return HttpResponse('<h1>Hello {} de {} anos</h1>'.format(nome, idade))
+
